82e1cd7a0630d38b81ae665174d2ad628eadb131
Shad <144332050+Shadrachanwhere@users.noreply.github.com>
2023-09-19 09:18:46 +0100

    commenting

--- guessing_game.py
+++ guessing_game.py
@@ -11,18 +11,18 @@ import statistics
 guess_list = []
 
 
-def stats(stats_of_guess):
+def stats(stats_of_guess):  # function to calculate the mean, mode, and the median
     print("=+Overall statistics+=")
     print(f"mean : {round(statistics.mean(stats_of_guess))}")
     print(f"mode: {statistics.mode(stats_of_guess)}")
     print(f"median: {statistics.median(stats_of_guess)}")
 
 
-def guesses(new_guess):
+def guesses(new_guess):  # function for adding a new input to a list
     guess_list.append(new_guess)
 
 
-def start_game():
+def start_game():  # game function
     high_score = 0
     print("Welcome to **Guess** **That** **Number**")
     print("Guess a number between 1 and 100")
@@ -44,14 +44,14 @@ def start_game():
                 print("You got it")
                 break
 
-    attempts = len(guess_list)
+    attempts = len(guess_list) # this code counts the number of guesses
     high_score = attempts
     print(f"Way to go, you guessed it in {attempts} tries")
-    stats(guess_list)
+    stats(guess_list) # function call on statistics
     play_again = input("Would you like to play again? [y/n]? ").lower()
     if play_again == "y":
         print(f"can you beat the high score of {high_score}")
-        guess_list.clear()
+        guess_list.clear()  # This function clears the list
         start_game()
     else:
         print("Closing the game! Thanks for playing")
